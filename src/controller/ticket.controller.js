3f3ebacdfa7197591f8753e3298e6da6c7dc3bc8
Catalina Riquelme <riquelmecata@gmail.com>
2024-02-04 14:44:44 -0300

    ticket

--- src/controller/ticket.controller.js
+++ src/controller/ticket.controller.js
@@ -1,11 +1,18 @@
 import TicketManager from "../DAL/dao/ticketManager.js";
+import { UserModel } from "../DAL/db/models/user.model.js";
 const Ticket = new TicketManager()
 
 export const postTicket = async (req, res) => {
     const { cid } = req.params
+
     if (!cid) return res.status(400).json({ status: "error", error: "Cid must be provided by params" })
+
     try {
-        const ticket = await Ticket.createTicket(cid)
+        const { ticket} = await Ticket.createTicket(cid)
+        // Obtener el usuario actual
+        const currentUser = await UserModel.findOne({ cart: cid });
+        currentUser.tickets.push(ticket);
+        await currentUser.save();
         return res.status(201).json({ status: "success", payload: ticket })
 
     } catch (error) {
